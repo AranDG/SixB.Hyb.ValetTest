e8a2741a1b5895a17876f51efa2fc6703d714e1c
Aran <arandgray@gmail.com>
2023-09-18 02:24:41 +0100

    Expected Service & Repository Interfaces

---
+++ SixB.Hyb.ValetTest.Core/Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,17 @@
+﻿using SixB.Hyb.ValetTest.Core.Models.DTOs;
+using SixB.Hyb.ValetTest.Core.Models;
+
+namespace SixB.Hyb.ValetTest.Core.Interfaces.Repositories
+{
+    public interface IBookingRepository
+    {
+        Task<IList<BookingDto>> Get();
+        Task<BookingDto> Get(int id);
+        Task Create(BookingDto booking);
+        Task Edit(BookingDto booking);
+        Task Delete(int id);
+        Task Approve(int id);
+        Task<IList<FlexibilityDay>> GetFlexibilityDays();
+        Task<IList<VehicleSize>> GetVehicleSizes();
+    }
+}
---
+++ SixB.Hyb.ValetTest.Core/Interfaces/Services/IBookingService.cs
@@ -0,0 +1,17 @@
+﻿using SixB.Hyb.ValetTest.Core.Models.ViewModels;
+using SixB.Hyb.ValetTest.Core.Models;
+
+namespace SixB.Hyb.ValetTest.Core.Interfaces.Services
+{
+    public interface IBookingService
+    {
+        Task<IList<BookingViewModel>> Get();
+        Task<BookingViewModel?> Get(int id);
+        Task Create(BookingViewModel booking);
+        Task Delete(int id);
+        Task Approve(int id);
+        Task Edit(BookingViewModel booking);
+        Task<IList<FlexibilityDay>> GetFlexibilityOptions();
+        Task<IList<VehicleSize>> GetVehicleSizeOptions();
+    }
+}
